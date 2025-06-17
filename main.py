0419bb772b40c2308f5e09ab664d1c0fa0a98185
Argo1a2a3a <charlie.adams7@education.nsw.gov.au>
2025-06-17 03:09:54 +0000

    e

---
+++ main.py
@@ -0,0 +1,7 @@
+from cave import Cave
+cavern = Cave("Cavern")
+cavern.set_description("A dank and dirty cave ")
+dungeon = Cave("Dungeon")
+dungeon.set_description("A large cave with a rack")
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
